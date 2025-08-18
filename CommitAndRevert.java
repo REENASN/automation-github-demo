f4065ef24554a5b293c7da50b1266972f5210159
REENASN <reenasn83@gmail.com>
2025-08-18 11:28:56 +0530

    Revert "new line added"
    This reverts commit 122d2c0dd418dc6f1fde471590ef6055601148a3.

--- CommitAndRevert.java
+++ CommitAndRevert.java
@@ -3,7 +3,7 @@ package Automation;
 public class CommitAndRevert {
 
 	public static void main(String[] args) {
-		System.out.println("new line added");
+		// TODO Auto-generated method stub
 
 	}
 
