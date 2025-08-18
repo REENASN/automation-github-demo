122d2c0dd418dc6f1fde471590ef6055601148a3
REENASN <reenasn83@gmail.com>
2025-08-18 11:26:56 +0530

    new line added

--- CommitAndRevert.java
+++ CommitAndRevert.java
@@ -3,7 +3,7 @@ package Automation;
 public class CommitAndRevert {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("new line added");
 
 	}
 
